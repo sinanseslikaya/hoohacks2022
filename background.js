992f5b8d9e9e764da852dca830d10b1996b18c06
sinanseslikaya <ss6gw@virginia.edu>
2022-03-26 13:38:23 -0400

    some stuff

--- background.js
+++ background.js
@@ -6,3 +6,13 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.tabs.onUpdated.addListener(function
+  (tabId, changeInfo, tab) {
+    // read changeInfo data and do something with it (like read the url)
+    if (changeInfo.url) {
+      // do something here
+
+    }
+  }
+);
