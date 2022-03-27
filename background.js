62280cc964b7daa20effdb8562685765cb5fe724
anish3343 <43268896+anish3343@users.noreply.github.com>
2022-03-26 20:15:43 -0400

    Icon Working

--- background.js
+++ background.js
@@ -11,16 +11,20 @@ let durl = 'https://www.dailymail.co.uk/ushome/index.html';
  * @param {string} url website url
  * @returns {string} see biasToColor(), filename for extension logo change
  */
-const newsSources = require('./media.json');
+// const newsSources = require('./media.json');
+const newsSources = fetch("./media.json").then(response => response.json()).then(data => {return data});
 // console.log(newsSources);
-function getLogo(url){
-    for (var site of newsSources){
+const getLogo = async (url) => {
+  const source = await newsSources;
+  url = url.slice(6);
+    for (var site of source){
         // console.log(site.url);
         if (site.url.includes(url) || url.includes(site.url)){
             // console.log(site.url);
             return "/images/" + biasToLogo(site.bias_rating) + confidenceToLogo(site.factual_reporting_rating) + ".png";
         }
     }
+    return "";
 }
 
 /**
@@ -28,13 +32,13 @@ function getLogo(url){
  * @param {string} bias the bias of the provided url.
  * @return {string} political bias prefix for logo retrieval.
  */
-function biasToLogo(bias){
-    bias = Number(bias);
-    if (-30 <= bias <-18){return "L";}
-    else if (-18 <= bias < -6) {return "LC";}
-    else if (-6 <= bias < 6) {return "C";}
-    else if (6 <= bias < 18) {return "RC";}
-    else {return "R"}
+ function biasToLogo(bias){
+  bias = Number(bias);
+  if (-30 <= bias  && bias < -18){return "L";}
+  else if (-18 <= bias && bias  < -6) {return "LC";}
+  else if (-6 <= bias && bias  < 6) {return "C";}
+  else if (6 <= bias && bias < 18) {return "RC";}
+  else {return "R"}
 }
 /**
  * Helper function for getLogo
@@ -74,13 +78,27 @@ chrome.tabs.onUpdated.addListener(function
       // else {
       //   chrome.action.setIcon({path: "/images/green.png", tabId :tabId});
       // }
-      let logo = getLogo(tab.url);
-      console.log(logo);
-      if (logo=='') {
-          chrome.action.setIcon({path: "/images/green.png", tabId : tabId});
-      } else {
-          chrome.action.setIcon({path: logo, tabId: tabId});
-      }
+
+
+      setIcon(tab, tabId);
+
+      // const logo = await getLogo(tab.url);
+      // console.log(logo);
+      // if (logo=='') {
+      //     chrome.action.setIcon({path: "/images/green.png", tabId : tabId});
+      // } else {
+      //     chrome.action.setIcon({path: ""+logo, tabId: tabId});
+      // }
 
     }
   });
+
+const setIcon = async(tab, tabId) => {
+  const logo = await getLogo(tab.url);
+  console.log(logo);
+  if (logo=='') {
+      chrome.action.setIcon({path: "/images/green.png", tabId : tabId});
+  } else {
+      chrome.action.setIcon({path: ""+logo, tabId: tabId});
+  }
+}
